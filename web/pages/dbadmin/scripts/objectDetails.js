3ac5517441b3d62a427f16208c41ba75fbd4eec0
Brian Zoetewey <brian.zoetewey@ccci.org>
2013-03-22 00:25:33 -0400

    Initial objectDetails controller and view.

--- web/pages/dbadmin/scripts/objectDetails.js
+++ web/pages/dbadmin/scripts/objectDetails.js
@@ -35,30 +35,63 @@
                 var options = $.extend(defaults, options);
                 this._super(el, options);
                 
-                
                 this.options = options;
-                
+                var self = this;
+
+                this.selectedModel = null;
+                this.addForm = null;
                 
                 // insert our DOM elements
                 this.insertDOM();
-                
-                
-                // attach other widgets & functionality here:
-                
-                
+
+                this.model = new hris.Object();
+                this.addForm.ad_form({
+                    dataManager: this.model,
+                    dataValid: this.isValid,
+                    error: '.text-error',
+                    submit: 'button.submit',
+                    cancel: 'button.cancel',
+                    onSubmit: this.onSubmit,
+                    onCancel: function() {
+                        console.log( 'Canceled' );
+                        return false;
+                    }
+                });
+
+                this.ADForm = this.addForm.data( 'ADForm' );
+
+                this.element.hide();
                 
                 // translate Labels
                 // any DOM element that has an attrib "appdLabelKey='xxxx'" will get it's contents
                 // replaced with our Label.  Careful to not put this on places that have other content!
                 this.xlateLabels();
             },
-            
 
+            isValid: function( data ) {
+                return true;
+            },
+
+            onSubmit: function( model ) {
+                console.log( model );
+                model.save( function( data ) {
+                    console.log( data );
+                } );
+            },
+
+            refreshData: function( model ) {
+                this.selectedModel = model;
+                this.ADForm.setModel( model );
+                this.element.show();
+            },
             
             insertDOM: function() {
-                
-                this.element.html(this.view('/hris/dbadmin/view/objectDetails.ejs', {}));
-                
+                this.element.html( this.view( '/hris/dbadmin/view/objectDetails.ejs', {} ) );
+                this.addForm = $( 'form', this.element );
+            },
+
+            'dbadmin.object.item.selected subscribe': function( msg, model ) {
+                this.refreshData( model );
             }
             
             
