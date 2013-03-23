075eed226e19742ccf0fe956ad45592e28b49e41
Jon Vellacott <JonVellacott@agape.org.uk>
2013-03-23 03:17:32 +0000

    Fixed undefined labels in carousel

--- models/Person.js
+++ models/Person.js
@@ -22,7 +22,7 @@
 				_adModule:'hris',
 				_adResource:'person',	// singular
 //				_adModel: [ModelName]   // <-- if the data returned is associated with a diff Model obj, provide it's name here:  _adModel:site.Label,
-				labelKey:'person_label',
+				labelKey:'person_surname',
 				id:'person_id'  // the field that is the id of the data
 		  };
 		  
