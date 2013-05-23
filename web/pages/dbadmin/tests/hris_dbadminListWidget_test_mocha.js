a18aa5ecb89108377c257f538ab8119e10599f4e
Joshua Chan <joshua@appdevdesigns.net>
2013-05-23 02:06:25 -0700

    Unit tests for dbadminListWidget now uses the generic dummy model.

--- web/pages/dbadmin/tests/hris_dbadminListWidget_test_mocha.js
+++ web/pages/dbadmin/tests/hris_dbadminListWidget_test_mocha.js
@@ -1,60 +1,33 @@
 
 describe('dbadminListWidget', function() {
 
+    // Using a dummy model instead of a real one from HRIS, so we can test this
+    // widget in isolation.
     var fakeData = [
         { id: 1, value: 'ABC' },
         { id: 2, value: 'DEF' },
         { id: 3, value: 'GHI' }
     ];
-    
-    AD.Model.extend("fakeModel", {
-        id: 'id',
-        labelKey: 'value',
-        findAll: function(params, onSuccess, onError) {
-            var dfd = $.Deferred();
-            dfd.then(onSuccess).fail(onError);
-            
-            var data = [];
-            for (var i=0; i<fakeData.length; i++) {
-                data.push( new fakeModel(fakeData[i]) );
-            }
-            
-            dfd.resolve(data);
-            return dfd;
-        },
-        destroy: function(id, onSuccess, onError) {
-            var dfd = $.Deferred();
-            dfd.then(onSuccess).fail(onError);
-            for (var i=0; i<fakeData.length; i++) {
-                if (fakeData[i]['id'] == id) {
-                    fakeData.splice(i, 1);
-                    dfd.resolve({ success: true });
-                    break;
-                }
-            }
-            if (i >= fakeData.length) {
-                dfd.reject({ success: false });
-            }
-            return dfd;
-        }
-    }, {});
-    hris['APIfakeModel'] = fakeModel;
-    
+    AD.DummyModel.fakeData = fakeData;
+    hris['APIfakeModel'] = AD.DummyModel;
     
     
     var $container;
     var controller;
     
+
     before(function(done) {
         $container = $('<div></div>');
         $(document).append($container);
         
+        // Initialize the widget with the dummy model we set up above
         $container.dbadmin_list_widget({
             modelName: 'fakeModel'
         });
         controller = $container.controller();
         done();
     });
+
     
     it('initializes the DOM', function(done) {
         // The dbadminListWidget is supposed to create an additional DIV inside
@@ -64,12 +37,14 @@ describe('dbadminListWidget', function() {
         done();
     });
     
+
     it('initializes the admin list controller', function(done) {
         chai.assert.instanceOf(controller.listController, appdevListAdmin, "appdevListAdmin widget controller not set");
         done();
     });
     
-    // This test might be more appropriate for the appdevListAdmin widget
+
+    // This test might be more appropriate for the appdevListAdmin sub-widget
     it('populates the list with correct items', function(done) {
         controller.refresh({});
         var $items = $container.find('li.appdev-list-admin-entry span.model-name');
@@ -85,8 +60,13 @@ describe('dbadminListWidget', function() {
         
         done();
     });
+    
 
-    
-    
+    after(function(done) {
+        $container.remove();
+        delete $container;
+        delete controller;
+        done();
+    });
     
 });
